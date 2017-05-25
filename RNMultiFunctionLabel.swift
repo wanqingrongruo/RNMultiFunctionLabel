3b6fd1e0d0634a7694139b0cf2b6acc28b54b7a5
婉卿容若 <devzheng@qq.com>
2017-05-25 13:15:35 +0800

    关键词高亮

--- RNMultiFunctionLabel.swift
+++ RNMultiFunctionLabel.swift
@@ -11,18 +11,84 @@ import UIKit
 
 class RNMultiFunctionLabel: UILabel {
     
+    enum TapGestureAction {
+        case dailPhone
+        case other
+    }
+    
+    var isOpenTapGesture: Bool = false // 是否打开点击手势 -- default: false
+    var isOpenLongGesture: Bool = true // 是否打开长按手势 -- default: true
+    
+    var isOpenHightLightForKeyword: Bool = false // 是否打开关键词高亮显示
+    var keyword: String? = nil // 关键词
+    
+    var tapAction: TapGestureAction = .dailPhone // 点击手势行为
+    
     override var canBecomeFirstResponder: Bool {
         return true
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.addLongPressGesture()
+        
+        if isOpenTapGesture {
+            self.addTapGesture()
+        }
+        
+        if isOpenLongGesture {
+            self.addLongPressGesture()
+        }
+        
+        if isOpenHightLightForKeyword {
+            
+            guard let k = keyword else {
+                return
+            }
+        
+            guard let t = text else {
+                return
+            }
+            
+            let attr = NSMutableAttributedString(string: t)
+            let str = NSString(string: t)
+            let theRange = str.range(of: k)
+            attr.addAttribute(NSForegroundColorAttributeName, value: UIColor.orange, range: theRange)
+            self.attributedText = attr
+            
+            
+        }
+        
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.addLongPressGesture()
+        
+        if isOpenTapGesture {
+            self.addTapGesture()
+        }
+        
+        if isOpenLongGesture {
+            self.addLongPressGesture()
+        }
+        
+        if isOpenHightLightForKeyword {
+            
+            guard let k = keyword else {
+                return
+            }
+            
+            guard let t = text else {
+                return
+            }
+            
+            let attr = NSMutableAttributedString(string: t)
+            let str = NSString(string: t)
+            let theRange = str.range(of: k)
+            attr.addAttribute(NSForegroundColorAttributeName, value: UIColor.orange, range: theRange)
+            self.attributedText = attr
+            
+        }
+
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -30,15 +96,20 @@ class RNMultiFunctionLabel: UILabel {
     }
     
     override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-        if action == #selector(copy(_:)){
+        if action == #selector(copyText(_:)){
             return true
         }else{
             return false
         }
         
     }
-   
+    
     override func copy(_ sender: Any?) {
+        let pastBoard = UIPasteboard.general
+        pastBoard.string = self.text
+    }
+    
+    func copyText(_ sender: Any?){
         let pastBoard = UIPasteboard.general
         pastBoard.string = self.text
     }
@@ -59,18 +130,48 @@ extension RNMultiFunctionLabel {
     func longPressAction(gesture: UILongPressGestureRecognizer){
         
         if gesture.state == .began{
-        
+            
             self.becomeFirstResponder()
             
-            //let copyItem = UIMenuItem(title: "复制", action: #selector(copy(_:)))
+            let copyItem = UIMenuItem(title: "复制", action: #selector(copyText(_:)))
             let menuVC = UIMenuController.shared
-           // menuVC.menuItems = [copyItem]
+            menuVC.menuItems = [copyItem]
             if menuVC.isMenuVisible {
                 return
             }
             menuVC.setTargetRect(bounds, in: self)
             menuVC.setMenuVisible(true, animated: true)
         }
-       
+        
+    }
+    
+    func addTapGesture() {
+        self.isUserInteractionEnabled = true
+        
+        let tapGesture = UITapGestureRecognizer.init(target: self, action: #selector(tapGetstureAction(_:)))
+        self.addGestureRecognizer(tapGesture)
+        
+    }
+    
+    func tapGetstureAction(_ gesture: UITapGestureRecognizer) {
+        
+        
+        switch tapAction {
+        case .dailPhone:
+            guard let title = self.text else{
+                return
+            }
+            
+            let telephoneNum = "telprompt://\(title)"
+            guard let tel = URL(string: telephoneNum) else{
+                return
+            }
+            UIApplication.shared.openURL(tel)
+            
+        default:
+            break
+        }
+        
+        
     }
 }
